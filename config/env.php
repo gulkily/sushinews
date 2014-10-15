9888d67d562791a05fc4461a3223ab4a7bc20767
gulkily <ilya@ilyagulko.com>
2014-10-14 20:38:03 -0400

    remove env.php, add env.default.php

--- config/env.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('GUID_SEED', "dsfadsfadsfadsfq43rq4rqfx4rdfadsf");
-define('CACHE_PATH', "/home/ilya/sushi/cache");
-define('SITE_NAME', '');
-define('SECRET_SALT', '4rx34rfadjkfadslfjklaewfj19895489fj83rfj8');
-define('SITE_DOMAIN', 'sushi.local');
-define('SITE_PATH', '/');
-define('SITE_PREFIX', 'http://');
