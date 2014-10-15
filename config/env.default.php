ef5130cd985372911b6b22373f4f0f4d529ba3a2
gulkily <ilya@ilyagulko.com>
2014-10-15 18:24:06 -0400

    add some comments to the config values

--- config/env.default.php
+++ config/env.default.php
@@ -21,7 +21,8 @@ define('SITE_NAME', '');
 define('SITE_PATH', '/');
 define('SITE_PREFIX', 'http://');
 
+// Set this to localhost if using sockets or 127.0.0.1 if using TCP
+define("EZSQL_DB_HOST", "localhost");        // <-- mysql server host
 define("EZSQL_DB_USER", "root");                 // <-- mysql db user
 define("EZSQL_DB_PASSWORD", "admin");               // <-- mysql db password
-define("EZSQL_DB_HOST", "localhost");        // <-- mysql server host
 define("EZSQL_DB_NAME", "sushinews");         // <-- mysql db pname
