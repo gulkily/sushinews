d55fb9239957baab026ea3f22f1455433b828e8e
gulkily <ilya@ilyagulko.com>
2014-10-15 18:20:07 -0400

    move sql config to env.php

--- config/env.default.php
+++ config/env.default.php
@@ -9,3 +9,8 @@ define('SECRET_SALT', '4rx34rfadjkfadslfjklaewfj19895489fj83rfj8');
 define('SITE_DOMAIN', 'sushi.local');
 define('SITE_PATH', '/');
 define('SITE_PREFIX', 'http://');
+
+define("EZSQL_DB_USER", "root");                 // <-- mysql db user
+define("EZSQL_DB_PASSWORD", "admin");               // <-- mysql db password
+define("EZSQL_DB_HOST", "localhost");        // <-- mysql server host
+define("EZSQL_DB_NAME", "sushinews");         // <-- mysql db pname
--- module/ez_sql.php
+++ module/ez_sql.php
@@ -1,9 +1,4 @@
 <?
-        define("EZSQL_DB_USER", "root");                 // <-- mysql db user
-        define("EZSQL_DB_PASSWORD", "admin");               // <-- mysql db password
-        define("EZSQL_DB_HOST", "localhost");        // <-- mysql server host
-        define("EZSQL_DB_NAME", "sushinews");         // <-- mysql db pname
-
         include_once('ez_sql_core.php');
         include_once('ez_sql_mysql.php');
 
