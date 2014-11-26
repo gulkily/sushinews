6273cef2a052ade88d2af639b366eaa969f18965
gulkily <ilya@ilyagulko.com>
2014-11-25 19:03:36 -0500

    add update_scores task create getRandomTask() function

--- module/tasks.php
+++ module/tasks.php
@@ -9,7 +9,24 @@ function doTask($taskName) {
             writeMysqlDump('./mirror/', 'sushinews');
 
             break;
+
+        case 'update_scores':
+            updateAllItemScores(1000); //@todo needs the limit in the function
+
+            break;
+
         default:
             break;
     }
+}
+
+function getRandomTask() {
+    $possibleTasks = array(
+        'export',
+        'update_scores'
+    );
+
+    $task = array_rand($possibleTasks);
+
+    return $task;
 }
