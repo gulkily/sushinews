3aa24d606f687fdf1ddeb7c6c0837ddc252f4201
gulkily <ilya@ilyagulko.com>
2014-12-03 09:06:02 -0500

    fleshing out the about page

--- template/about.php
+++ template/about.php
@@ -7,7 +7,13 @@ function printAbout() {
 <div class="large-12 columns">
 <div class="panel">
 
-            <p>Hello, World!</p>
+    <p>Hello, World!</p>
+
+    <p>This is a text-based wiki-like site with open submission and semi-open moderation.</p>
+
+    <p><a href="https://www.github.com/gulkily/sushinews">The source code is available on GitHub</a>, though I must warn you that it's dirty PHP.</p>
+
+    <p><a href="/mirror/">Data dumps for your mirroring pleasure.</a></p>
 
 </div>
 </div>
