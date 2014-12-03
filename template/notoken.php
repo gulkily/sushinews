a7de14903a65b7d5d57ab899a00a9fd42b0f2239
gulkily <ilya@ilyagulko.com>
2014-12-03 03:30:23 -0500

    create template page for when voter id is missing

---
+++ template/notoken.php
@@ -0,0 +1,13 @@
+<?php
+
+function printNoToken() {
+
+    ?>
+    <div class="row">
+        <div class="large-12 columns">
+            Sorry, but you cannot moderate because you don't have a token.
+        </div>
+    </div>
+<?php
+}
+?>
