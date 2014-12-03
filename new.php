832ce0e02c60f68b734baec4bb21a24c4ed522f3
gulkily <ilya@ilyagulko.com>
2014-12-03 02:56:00 -0500

    reformat the body auto-generate a summary if there is none

--- new.php
+++ new.php
@@ -3,11 +3,36 @@ include_once('module/utilities.php');
 include_once('module/items.php');
 
     if (isset($_POST)) {
-        $title = getParam('title');
-        $summary = getParam('summary');
-        $body = getParam('body');
+        $title = trim(getParam('title'));
+        $summary = trim(getParam('summary'));
+        $body = trim(getParam('body'));
         $guid = getParam('guid');
         $parent_id = getParam('parentstory');
+
+        if (!$summary && $body) {
+            $body_lines = explode("\n", trim($body));
+            $summary = $body_lines[0];
+
+            $body_new = '';
+
+            $comma = 0;
+
+            foreach ($body_lines as $line) {
+                $line = trim($line);
+
+                if ($line) {
+                    if ($comma) {
+                        $body_new .= "\n\n";
+                    } else {
+                        $comma = 1;
+                    }
+
+                    $body_new .= $line;
+                }
+            }
+
+            $body = $body_new;
+        }
 
         if ($title && $summary && $body) {
             $newItemId = createNewItem($title, $summary, $body, $parent_id, $guid);
