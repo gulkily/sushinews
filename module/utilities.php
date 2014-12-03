e25db9115eeff6070db73a9f1d477489b8e5bdf5
gulkily <ilya@ilyagulko.com>
2014-12-03 06:46:12 -0500

    move voting-related stuff into voting.php

--- module/utilities.php
+++ module/utilities.php
@@ -3,7 +3,7 @@
 include_once('config/env.php');
 include_once('module/ez_sql.php');
 include_once('module/cache.php');
-
+include_once('module/voting.php');
 
 function getParam($name) {
     if (isset($_GET[$name])) {
@@ -16,69 +16,7 @@ function getParam($name) {
 
     return $param;
 }
-//
-//function getClientVotes() {
-//    if (!$_COOKIE['voted']) {
-//        return array();
-//    } else {
-//
-//    }
-//}
 
-function getVotingHash($client_id, $item_id, $tag) {
-    return md5($client_id . $item_id . $tag . SECRET_SALT);
-}
-
-function getVoterId() {
-    static $voter;
-
-    if (isset($_COOKIE['voter_id'])) {
-        $voter_id = $_COOKIE['voter_id'];
-
-        $voter = explode(',', $voter_id);
-    }
-
-    // @todo some sanity checks here
-    if ($voter[1] == md5($voter[0] . $_SERVER['REMOTE_ADDR'] . SECRET_SALT)) {
-        return $voter[1];
-    } else {
-        return 0;
-    }
-}
-
-function setVoterIdCookie($voter_id) {
-    $cookie = $voter_id . ',' . md5($voter_id . $_SERVER['REMOTE_ADDR'] . SECRET_SALT);
-
-    if (!headers_sent()) setcookie('voter_id', $cookie);
-}
-
-function generateVoterId() {
-    $host = $_SERVER['REMOTE_ADDR'];
-    $host .= round(time() / 3600);
-    $host = md5($host);
-
-    global $db;
-    $last_assignment = $db->get_var("SELECT last_assignment FROM voter_id_rate WHERE host = '$host' AND DATE_ADD(last_assignment, INTERVAL 15 SECOND) > NOW()"); // @todo escape
-
-    if ($last_assignment === null) {
-        $query = "INSERT INTO voter_id_rate(host, last_assignment) VALUES('$host', NOW())";
-        $db->query($query);
-
-        $voter_id = md5(rand(0,100000) . time()); // @todo make this more random
-
-        return $voter_id;
-    } else {
-        return null;
-    }
-}
-
-function verifyVotingHash($client_id, $item_id, $tag, $hash) {
-    if (md5($client_id . $item_id . $tag . SECRET_SALT) == $hash) {
-        return true;
-    } else {
-        return false;
-    }
-}
 //
 //function getConfigLine($name, $value) {
 //    return "define ('" . $name . "','" . $value . "');\n";
@@ -209,10 +147,6 @@ function addTagToItem($item_id, $tag_name, $voter_id) {
     $stmt = $dbp->prepare("INSERT INTO item_tag(item_id, tag_id, voter_id) VALUES(:item_id, :tag_id, :voter_id)");
 
     $stmt->execute(array(':item_id' => $item, ':tag_id' => $tag_id, ':voter_id' => $voter_id));
-}
-
-function getUserId() {
-    return 1;
 }
 
 function getOwnUrl() {
---
+++ module/voting.php
@@ -0,0 +1,57 @@
+<?php
+
+
+function getVotingHash($client_id, $item_id, $tag) {
+    return md5($client_id . $item_id . $tag . SECRET_SALT);
+}
+
+function getVoterId() {
+    static $voter;
+
+    if (isset($_COOKIE['voter_id'])) {
+        $voter_id = $_COOKIE['voter_id'];
+
+        $voter = explode(',', $voter_id);
+    }
+
+    // @todo some sanity checks here
+    if ($voter[1] == md5($voter[0] . $_SERVER['REMOTE_ADDR'] . SECRET_SALT)) {
+        return $voter[1];
+    } else {
+        return 0;
+    }
+}
+
+function setVoterIdCookie($voter_id) {
+    $cookie = $voter_id . ',' . md5($voter_id . $_SERVER['REMOTE_ADDR'] . SECRET_SALT);
+
+    if (!headers_sent()) setcookie('voter_id', $cookie);
+}
+
+function generateVoterId() {
+    $host = $_SERVER['REMOTE_ADDR'];
+    $host .= round(time() / 3600);
+    $host = md5($host);
+
+    global $db;
+    $last_assignment = $db->get_var("SELECT last_assignment FROM voter_id_rate WHERE host = '$host' AND DATE_ADD(last_assignment, INTERVAL 15 SECOND) > NOW()"); // @todo escape
+
+    if ($last_assignment === null) {
+        $query = "INSERT INTO voter_id_rate(host, last_assignment) VALUES('$host', NOW())";
+        $db->query($query);
+
+        $voter_id = md5(rand(0,100000) . time()); // @todo make this more random
+
+        return $voter_id;
+    } else {
+        return null;
+    }
+}
+
+function verifyVotingHash($client_id, $item_id, $tag, $hash) {
+    if (md5($client_id . $item_id . $tag . SECRET_SALT) == $hash) {
+        return true;
+    } else {
+        return false;
+    }
+}
