991bbd3e9f92953410e421beafac4ca4defbe70d
gulkily <ilya@ilyagulko.com>
2014-12-03 08:14:23 -0500

    include the publish timestamp in unix timestamp format in the results of getItem()

--- module/items.php
+++ module/items.php
@@ -143,7 +143,7 @@ function getItem($item_id) {
     $itemId = intval($item_id);
     if (!$itemId) return;
 
-    $stmt = $dbp->prepare("SELECT title, body, summary, id, guid, publish_timestamp FROM item WHERE id=:id");
+    $stmt = $dbp->prepare("SELECT title, body, summary, id, guid, publish_timestamp, UNIX_TIMESTAMP(publish_timestamp) publish_timestamp_u FROM item WHERE id=:id");
     $stmt->bindParam(':id', $itemId);
 
     $item = get_cache_dbp("item/$itemId", 60, $stmt);
@@ -171,6 +171,8 @@ function getItem($item_id) {
             'id' => $row['id'],
             'tags' => $itemTags,
             'publish_timestamp' => $row['publish_timestamp'],
+            'publish_timestamp_u' => $row['publish_timestamp_u'],
+            'publish_timestamp_u' => $row['publish_timestamp_u'],
             'guid' => $row['guid']
         );
     }
