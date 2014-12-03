8a9a1788fef04468f8c151f8ca1c0e93b4134ce6
gulkily <ilya@ilyagulko.com>
2014-12-03 07:10:41 -0500

    edit message

--- template/notoken.php
+++ template/notoken.php
@@ -6,11 +6,11 @@ function printNoToken() {
     <div class="row">
         <div class="large-12 columns">
             <p>
-            Sorry, but you cannot moderate because you don't have a token.
+                In order to moderate, you have to get a token.
             </p>
 
             <p>
-            <a href="/?action=moderate&token=welcome">Get Token</a>
+                <a href="/?action=moderate&token=welcome">Get Token</a>
             </p>
         </div>
     </div>
