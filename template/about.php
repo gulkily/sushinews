28bec12ee92799ea584b45d31fe60920e028f495
gulkily <ilya@ilyagulko.com>
2014-12-03 06:40:22 -0500

    make about page prettier

--- template/about.php
+++ template/about.php
@@ -4,9 +4,13 @@ function printAbout() {
 
 ?>
 <div class="row">
-    <div class="large-12 columns">
-        Hello, World!
-    </div>
+<div class="large-12 columns">
+<div class="panel">
+
+            <p>Hello, World!</p>
+
+</div>
+</div>
 </div>
 <?php
 }
