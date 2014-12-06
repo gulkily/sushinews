6229cd8c498c4e324d129e28ebbc2c87d24827aa
gulkily <ilya@ilyagulko.com>
2014-12-06 13:11:45 -0500

    include config.php in index

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ if(!@include_once("config/env.php")) {
     throw new Exception("Please create config/env.php. You can use env.default.php as a starting point.");
 }
 
+include_once('config.php');
 include_once('module/utilities.php');
 //include_once('module/sherlock.php');
 include_once('module/items.php');
