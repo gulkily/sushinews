1297a4582690ccd61d8aa9ef77ce9cc6c708d9dc
gulkily <ilya@ilyagulko.com>
2015-03-05 14:45:09 -0500

    fleshing out database migrations

--- module/setup.php
+++ module/setup.php
@@ -1,23 +1,80 @@
 <?php
 
-function getTables() {
-    global $db;
+class SetupClass {
+    function getLastDatabaseVersion() {
+        return 2;
+    }
 
-    $tables = $db->get_col("SHOW TABLES");
+    function getTables() {
+        global $db;
 
-    return $tables;
-}
+        $tables = $db->get_col("SHOW TABLES");
 
-function populateDatabase() {
-    global $db;
+        return $tables;
+    }
 
-    $schema = file_get_contents(getConfigDefault('mirror_path') . '/sushinews_schema.sql');
+    function populateDatabase() {
+        global $db;
 
-    $queries = explode(';', $schema);
+        $schema = file_get_contents(getConfigDefault('mirror_path') . '/sushinews_schema.sql');
 
-    foreach ($queries as $query) {
+        $queries = explode(';', $schema);
+
+        foreach ($queries as $query) {
+            $db->query($query);
+        }
+
+        $this->setDatabaseVersion($this->getLastDatabaseVersion());
+
+        //@todo this is a work in progress, since it doesn't work for some mysterious reason
+    }
+
+    function getDatabaseVersion() {
+        global $db;
+
+        $query = "SELECT value FROM config WHERE key = 'db_version'";
+
+        $databaseVersion = intval($db->get_var($query));
+
+        return $databaseVersion;
+    }
+
+    function setDatabaseVersion($newVersion) {
+        global $db;
+
+        $newVersion = intval($newVersion);
+
+        $query = "INSERT INTO config(key, value) VALUES('db_version', $newVersion) ON DUPLICATE KEY UPDATE";
+
         $db->query($query);
     }
 
-    //@todo this is a work in progress, since it doesn't work for some mysterious reason
+    function upgradeDatabase($currentVersion, $newVersion) {
+
+        if ($currentVersion + 1 !== $newVersion) {
+            return;
+        }
+
+        $queries = array();
+
+
+        switch($currentVersion) {
+            case 1:
+                $queries[] = "ALTER TABLE `config` ADD PRIMARY KEY `key` (`key`), DROP INDEX `key`;";
+                break;
+            case 2:
+                $queries[] = "ALTER TABLE `item_tag` ADD `vote_timestamp` datetime NOT NULL AFTER `voter_id`;";
+                $queries[] = "ALTER TABLE `item_tag` ADD UNIQUE `item_id_tag_id_voter_id` (`item_id`, `tag_id`, `voter_id`), DROP INDEX `item_id_tag_id_client_id`; ";
+                break;
+        }
+
+        global $db;
+
+        foreach ($queries as $query) {
+            $db->query($query);
+        }
+
+        setDatabaseVersion($newVersion);
+
+    }
 }
