c9870e36105a007f24d642832b72af7d11d45b48
gulkily <ilya@ilyagulko.com>
2015-07-20 18:40:10 -0400

    mysql is optional

--- module/ez_sql.php
+++ module/ez_sql.php
@@ -1,4 +1,5 @@
 <?
+    if (defined(EZSQL_DB_HOST)) {
         include_once('ez_sql_core.php');
         include_once('ez_sql_mysql.php');
 
@@ -11,4 +12,7 @@
         $db = new ezSQL_mysql(EZSQL_DB_USER, EZSQL_DB_PASSWORD, EZSQL_DB_NAME, EZSQL_DB_HOST);
 
         $dbp = new PDO('mysql:host=' . EZSQL_DB_HOST . ';dbname=' . EZSQL_DB_NAME . ';charset=utf8', EZSQL_DB_USER, EZSQL_DB_PASSWORD);
+    } else {
+
+    }
 ?>
